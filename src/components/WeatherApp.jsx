e8e548c4e963937f7939dadc19196e659596ff30
sajan moon <sajanmoon5@gmail.com>
2023-10-04 15:33:36 +0530

    added images changing features

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -3,26 +3,25 @@ import "./WeatherApp.css"
 
 const WeatherApp = () => {
     const [city, setCity] = useState("")
-    const [search, setSearch] = useState("London")
-    const [cloudImage, setCloudImage] = useState("");   
+    const [search, setSearch] = useState("")
+    const [searchValue, setSearchValue] = useState("London");
 
     const getWeather = async () => {
 
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=e7badfb125aed48f90506b68715ded6f&units=metric`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=e7badfb125aed48f90506b68715ded6f&units=metric`
         const response = await fetch(url);
         const respJson = await response.json();
         setCity(respJson)
-
-        const temp = respJson.main && respJson.main.temp;
-        setCloudImage(getCloudImage(temp));
-
+        setSearch(respJson ? searchValue : respJson);
     }
 
-    const getCloudImage = (temp) => {
+    const getWeatherImage = temp => {
         if (temp > 25) {
-            return "./images/clear.png"; // Change this to your sunny cloud image
+            return "/images/clear.png";
+        } else if (temp > 15) {
+            return "/images/cloud.png";
         } else {
-            return "./images/cloud.png"; // Default cloud image
+            return "/images/drizzle.png";
         }
     };
 
@@ -36,32 +35,33 @@ const WeatherApp = () => {
         <>
             <div className="container">
                 <div className="inputBox">
-                    <input type="text" placeholder='Enter city' onChange={(e) => { setSearch(e.target.value) }} />
+                    <input type="text" placeholder='Enter city' name="search"
+                        value={searchValue} onChange={(e) => { setSearchValue(e.target.value) }} required />
                     <button onClick={getWeather}><i className="fa-solid fa-magnifying-glass"></i></button>
                 </div>
                 {city.name && (
-                
-                <div className="info">
-                    <img className='img' src={cloudImage} alt="cloud" />
-                    <h1 className="temp">{city.main && city.main.temp}°C</h1>
-                    <h1 className='location'>{search}</h1>
+
+                    <div className="info">
+                        <img className='img' src={getWeatherImage(city.main && city.main.temp)} alt="cloud" />
+                        <h1 className="temp">{city.main && city.main.temp}°C</h1>
+                        <h1 className='location'>{search}</h1>
 
 
-                    <div className="data-container">
-                        <div className="data">
-                            <i className="fa-solid fa-droplet"></i>
-                            <div className="text">Humidity</div>
-                            <div className="humidity">{city.main && city.main.humidity}%</div>
+                        <div className="data-container">
+                            <div className="data">
+                                <i className="fa-solid fa-droplet"></i>
+                                <div className="text">Humidity</div>
+                                <div className="humidity">{city.main && city.main.humidity}%</div>
 
-                        </div>
-                        <div className="data">
-                            <i className="fa-solid fa-wind"></i>
-                            <div className="text">Wind Speed</div>
-                            <div className="wind-speed">{city.wind && city.wind.speed}m/s</div>
+                            </div>
+                            <div className="data">
+                                <i className="fa-solid fa-wind"></i>
+                                <div className="text">Wind Speed</div>
+                                <div className="wind-speed">{city.wind && city.wind.speed}m/s</div>
 
+                            </div>
                         </div>
                     </div>
-                </div>
                 )
                 }
             </div >
